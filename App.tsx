55cba1d210f72d9613f98d2ec90af2c9aceccb2a
Shantanu Dubey <48353164+dubeyshantanu2@users.noreply.github.com>
2022-11-19 16:28:17 +0530

    Changed asests location and added loginscreen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Image style={{ height: 100, width: 100}} source={require("./src/assets/icon.png")}/>
       <StatusBar style="auto" />
     </View>
   );
