a7b836656b7c50fae8d3d64773e64d972084d8c8
Shantanu Dubey <48353164+dubeyshantanu2@users.noreply.github.com>
2022-11-19 16:33:24 +0530

    Added screens

---
+++ src/screens/ChooseScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ChooseScreen = () => {
+  return (
+    <View>
+      <Text>ChooseScreen</Text>
+    </View>
+  )
+}
+
+export default ChooseScreen
---
+++ src/screens/SelectTimeScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SelectTimeScreen = () => {
+  return (
+    <View>
+      <Text>SelectTimeScreen</Text>
+    </View>
+  )
+}
+
+export default SelectTimeScreen
